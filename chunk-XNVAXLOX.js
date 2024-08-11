36e91776f731ee24b761c9f19ec5ddff5176af3b
Rome-lo <romeo7mol@gmail.com>
2024-08-10 18:47:25 -0600

    Auto-generated commit

---
+++ chunk-XNVAXLOX.js
@@ -0,0 +1,1 @@
+import"./chunk-POLL2CVR.js";var o=[{path:"dashboard",children:[{path:"",loadComponent:()=>import("./chunk-52TFSNHO.js")},{path:"crear",loadComponent:()=>import("./chunk-RWQZZSLT.js")},{path:"edit/:clienteId",loadComponent:()=>import("./chunk-RWQZZSLT.js")}]},{path:"**",redirectTo:"dashboard"}];export{o as default};
